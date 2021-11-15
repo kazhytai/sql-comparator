c25edfd8c37330492f4a093750c6a73a9cc65e36
tor <kkazhytai@mp.kz>
2021-11-15 17:27:29 +0600

    composer.json update

--- src/SqlComparator/Comparison.php
+++ src/SqlComparator/Comparison.php
@@ -5,7 +5,7 @@
  * @license      Proprietary and confidential
  * Unauthorized copying or using of this file, via any medium is strictly prohibited.
  * Content can not be copied and/or distributed without the express permission of Real Time Engineering, LLP
- * @author       Written by Nurlan Mukhanov <nmukhanov@mp.kz>, октябрь 2021
+ * @author       Written by Kuatbek Kazhytai <kkazhytai@mp.kz>, октябрь 2021
  */
 
 declare(strict_types = 1);
